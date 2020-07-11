30acdf2d31a344af172471782bb09fec73275f30
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-11 12:03:04 -0600

    Add model tests

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Invoice, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :customer_id }
+    it { should validate_presence_of :merchant_id }
+    it { should validate_presence_of :status }
+  end
+
+  describe "relationships" do
+    it { should belong_to :customer }
+    it { should belong_to :merchant }
+    it { should have_many :invoice_items }
+    it { should have_many(:items).through(:invoice_items) }
+  end
+end
