f4b74d8f80ffb7636e0b4aa9eb707042835e80eb
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-11 15:02:55 -0600

    Update test to use serializer

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -10,6 +10,25 @@ describe "Items API" do
 
     items = JSON.parse(response.body)
 
-    expect(items.count).to eq(3)
+    expect(items).to be_instance_of(Hash)
+    expect(items.keys.first).to eq("data")
+    expect(items["data"]).to be_instance_of(Array)
+    expect(items["data"].count).to eq(3)
   end
+
+  it "can get one item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(item).to be_instance_of(Hash)
+    expect(item.keys.first).to eq("data")
+    expect(item["data"]["id"]).to eq("#{id}")
+  end
+
+  it ""
+
 end
