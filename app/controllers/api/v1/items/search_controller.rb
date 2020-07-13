d2b5c4a8bd911f0a44fe2da611e920e7ebabf243
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-13 12:47:38 -0600

    Add helper method and poro

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::Items::SearchController < ApplicationController
 
   def show
+    search = ItemSearch.new(query_params)
+    results = search.results
+    render json: ItemSerializer.new(Item.find(results))
+  end
+
+  private
+
+  def query_params
     query_keys = params.keys[0..-3]
     query_values = []
     query_keys.each { |key| query_values << params[key] }
     query_keys.zip(query_values)
-
   end
 
 end
