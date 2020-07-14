0a56d6028e0faa676dac2bf2ed1833679a02d46a
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-14 14:05:32 -0600

    Add controller for merchant search

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::Merchants::SearchController < ApplicationController
 
   def show
+    search = MerchantSearch.new(query_params)
+    results = search.results
+    render json: MerchantSerializer.new(Merchant.find(results))
+  end
 
+  private
+
+  def query_params
+    query_keys = params.keys[0..-3]
+    query_values = []
+    query_keys.each { |key| query_values << params[key] }
+    query_keys.zip(query_values)
   end
 
 end
