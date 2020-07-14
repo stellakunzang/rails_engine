6082598c6286cf1b2dc2c367f47b02e6fac57528
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-14 14:04:27 -0600

    Add test for item search with multiple queries

--- spec/requests/api/v1/items/search_request_spec.rb
+++ spec/requests/api/v1/items/search_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Items Search" do
-  it "can return a single item" do
+  it "can find items based on single query" do
     merchant = create(:merchant)
     5.times do
       create(:item, merchant: merchant)
@@ -31,5 +31,45 @@ describe "Items Search" do
     response = JSON.parse(body)
 
     expect(response["data"].count).to eq(2)
+  end
+
+  it "can find items based on mutliple queries" do
+    merchant = create(:merchant)
+    5.times do
+      create(:item, merchant: merchant)
+    end
+
+    item1_params = {
+                    "name": "Polly Pocket",
+                    "description": "Nostalgic toy",
+                    "merchant_id": "#{merchant.id}",
+                    "unit_price": "499"
+                  }
+
+    item2_params = {
+                    "name": "Tomagatchi",
+                    "description": "Retro toy",
+                    "merchant_id": "#{merchant.id}",
+                    "unit_price": "599"
+                    }
+
+    item3_params = {
+                    "name": "Squirt Gun",
+                    "description": "Water gun for fun in the sun!",
+                    "merchant_id": "#{merchant.id}",
+                    "unit_price": "899"
+                    }
+    item1 = Item.create(item1_params)
+    item2 = Item.create(item2_params)
+    item3 = Item.create(item3_params)
+
+    get '/api/v1/items/find?description=toy&name=squirt'
+
+    expect(response).to be_successful
+
+    body = response.body
+    response = JSON.parse(body)
+
+    expect(response["data"].count).to eq(3)
   end
 end
