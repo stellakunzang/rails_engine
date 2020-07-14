678798a87286b4be7080a8ff9f02b682dafcb9ca
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-13 18:09:14 -0600

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'merchants/find', to: 'merchants/search#show'
-      get 'merchants/most_revenue', to: 'merchants/revenue#show'
+      get 'merchants/most_revenue', to: 'merchants/revenue#index'
       get 'merchants/find', to: 'merchants/search#show'
       get 'items/find', to: 'items/search#show'
       resources :items, except: [:new, :edit] do
