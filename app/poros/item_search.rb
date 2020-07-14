3e4bcf13e84f7fdc3c8bde6c46e7e0a2ff5b25a5
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-14 14:04:55 -0600

    Fix find to not be case sensitive

--- app/poros/item_search.rb
+++ app/poros/item_search.rb
@@ -6,7 +6,7 @@ class ItemSearch
   def results
     query_results = []
     @query.each do |attribute, search_term|
-      query_results << Item.where("#{attribute} LIKE ?", "%#{search_term}%").pluck(:id)
+      query_results << Item.where("lower(#{attribute}) LIKE ?", "%#{search_term.downcase}%").pluck(:id)
     end
     query_results.flatten.uniq
   end
