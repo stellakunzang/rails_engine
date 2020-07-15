379d844750c528c1492d72b2f21b734f987f8d22
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-14 20:21:48 -0600

    Create routes for business intelligence endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get 'revenue', to: 'revenue#show'
       namespace :merchants do
         get 'find', to: 'search#show'
         get 'most_revenue', to: 'revenue#index'
+        get 'most_items', to: 'most_items#index'
         get 'find', to: 'search#show'
         get 'find_all', to: 'search#index'
       end
       resources :merchants, except: [:new, :edit] do
         get 'items', to: 'merchants/items#index'
+        get 'revenue', to: 'merchants/revenue#show'
       end
       namespace :items do
         get 'find', to: 'search#show'
