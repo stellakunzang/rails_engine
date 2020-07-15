df762d728606bdd68ed511efa13a78ea87ea4866
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-14 20:22:09 -0600

    Get revenue

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -6,4 +6,10 @@ class Api::V1::Merchants::RevenueController < ApplicationController
     render json: MerchantSerializer.new(Merchant.find([merchants.keys]))
   end
 
+  def show
+    merchant = Merchant.find(params[:merchant_id])
+    revenue = MerchantRevenue.new.total_revenue(merchant)
+    # revenue_params = {id: merchant.id, name: merchant.name, revenue: revenue}
+    render json: MerchantRevenueSerializer.new(merchant, {params: {revenue: revenue }})
+  end
 end
