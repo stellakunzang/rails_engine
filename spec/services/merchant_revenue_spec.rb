64dd1d663391fe6e3d017afad7fbd464c48cee04
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-15 15:49:53 -0600

    Add more tests

--- spec/services/merchant_revenue_spec.rb
+++ spec/services/merchant_revenue_spec.rb
@@ -10,5 +10,39 @@ describe MerchantRevenue do
       list_comparison = list.first[-1] > list.last[-1]
       expect(list_comparison).to eq(true)
     end
+
+    it '#total_revenue' do
+      merchant = create(:merchant)
+
+      10.times do
+        create(:invoice, merchant: merchant)
+      end
+
+      expectated = InvoiceItem.sum(:total)
+      total = MerchantRevenue.new.total_revenue(merchant)
+
+      expect(expectated).to eq(total)
+    end
+
+    it '#total_revenue_by_date' do
+      merchant = create(:merchant)
+      10.times do
+        create(:invoice, merchant: merchant)
+      end
+
+      merchant2 = create(:merchant)
+      5.times do
+        create(:invoice, merchant: merchant2)
+      end
+
+      start = "2020-07-13 00:00:00"
+      stop = "2020-09-26 23:59:59"
+
+      expected = InvoiceItem.sum(:total)
+      revenue_by_date = MerchantRevenue.new.total_revenue_by_date(start, stop)
+
+      expect(expected).to eq(revenue_by_date)
+    end
+
   end
 end
