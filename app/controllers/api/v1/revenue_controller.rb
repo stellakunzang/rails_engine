b61c0adb71bdbf6ec6206fcad7ccb37487c8996d
stellakunzang <stella.k.bonnie@gmail.com>
2020-07-15 15:22:34 -0600

    Fix date

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,15 +1,19 @@
 class Api::V1::RevenueController < ApplicationController
   def show
-    start = convert_date(params[:start])
-    stop = convert_date(params[:end])
+    start = beginning_of_day(params[:start])
+    stop = end_of_day(params[:end])
     total_revenue = MerchantRevenue.new.total_revenue_by_date(start, stop)
     render json: MerchantRevenueSerializer.new(total_revenue).info
   end
 
   private
 
-  def convert_date(date_params)
-    date = DateTime.parse(date_params)
-    date.strftime("%a, %d %b %Y")
+  def beginning_of_day(date)
+    date + " 00:00:00"
   end
+
+  def end_of_day(date)
+    date + " 23:59:59"
+  end
+
 end
